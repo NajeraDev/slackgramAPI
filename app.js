a556897f9bf6852fb7fb4f9c7f9b0af6e7e863cc
Emmanuel Najera <najeradev@gmail.com>
2021-07-15 23:02:43 -0500

    heroku deploy v0.6

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const { createEventAdapter } = require('@slack/events-api');
 
 const slackSigningSecret = process.env.SLACK_SIGNING_SECRET; 
 const slackToken = process.env.SLACK_TOKEN;
-const port = process.env.SLACK_PORT || 3000;
+const port = process.env.PORT;
 const telegramToken = process.env.TELEGRAM_TOKEN;
 
 const bot = new TelegramBot(telegramToken, {polling: true});
